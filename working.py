4867ffd333f1bbdfde0af7ba248ec51d5975dd36
schmam <35980147+schmam@users.noreply.github.com>
2022-09-04 11:36:19 -0500

    Update working.py

--- working.py
+++ working.py
@@ -26,8 +26,12 @@ def convert(s):
             return ValueError
 
         if match.group(3) == "AM":                  # if time in first position is AM, no need to add 12 hours
-            hour_1 = int(match.group(1))
+            if match.group(1) == "12":              # if 12:00 AM, return "00:00"
+                hour_1 = 0
+            else:
+                hour_1 = int(match.group(1))
             minute_1 = int(match.group(2))
+
         elif match.group(3) == "PM":                # add 12 hours if PM
             hour_1 = (12 + int(match.group(1)))
             minute_1 = int(match.group(2))
@@ -35,8 +39,12 @@ def convert(s):
             return ValueError
 
         if match.group(6) == "AM":                  # if time in first position is AM, no need to add 12 hours
-            hour_2 = int(match.group(4))
+            if match.group(4) == "12":              # if 12:00 AM, return "00:00"
+                hour_2 = 0
+            else:
+                hour_2 = int(match.group(4))
             minute_2 = int(match.group(5))
+
         elif match.group(6) == "PM":                # add 12 hours if PM
             hour_2 = (12 + int(match.group(4)))
             minute_2 = int(match.group(5))
@@ -44,6 +52,8 @@ def convert(s):
             return ValueError
 
         return f"{hour_1:02}:{minute_1:02} to {hour_2:02}:{minute_2:02}"
+
+# second format to look for is, e.g., "9 AM to 5 PM"
 
     elif match := re.search(r"([0-9]?[0-9]+) ([A-Z]+M) to ([0-9]?[0-9]+) ([A-Z]+M)", s, re.IGNORECASE):
 
@@ -53,14 +63,20 @@ def convert(s):
             return ValueError
 
         if match.group(2) == "AM":                  # if time in first position is AM, no need to add 12 hours
-            hour_1 = int(match.group(1))
+            if match.group(1) == "12":              # if 12:00 AM, return "00:00"
+                hour_1 = 0
+            else:
+                hour_1 = int(match.group(1))
         elif match.group(2) == "PM":                # add 12 hours if PM
             hour_1 = (12 + int(match.group(1)))
         else:
             return ValueError
 
         if match.group(4) == "AM":                  # if time in first position is AM, no need to add 12 hours
-            hour_2 = int(match.group(3))
+            if match.group(3) == "12":              # if 12:00 AM, return "00:00"
+                hour_2 = 0
+            else:
+                hour_2 = int(match.group(3))
         elif match.group(4) == "PM":                # add 12 hours if PM
             hour_2 = (12 + int(match.group(3)))
         else:
@@ -73,7 +89,6 @@ def convert(s):
         sys.exit("This is a test exit")
 
 
+
 if __name__ == "__main__":
     main()
-
-
